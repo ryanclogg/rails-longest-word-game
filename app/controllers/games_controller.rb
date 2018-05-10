74370e2ffc74b302f9dd5ff145166605f9408b3d
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-10 12:42:58 +0800

    Lunch break - letters working

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def new
+    @arr = []
+    10.times do|x|
+      random_letter = ('A'..'Z').to_a.sample
+      @arr << random_letter
+    end
+    @arr
+  end
+
+  def score
+  end
+end
